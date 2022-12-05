4e950f22df0d42085c1876057d92e9730bede240
IBNAHMADMOHAMED1 <ibnahamdmohamed8@gmail.com>
2022-12-05 14:27:29 +0100

    add Employee Service

--- src/main/java/com/employee/employeemanger/service/EmployeeService.java
+++ src/main/java/com/employee/employeemanger/service/EmployeeService.java
@@ -1,2 +1,24 @@
-package com.employee.employeemanger.service;public class EmployeeService {
+package com.employee.employeemanger.service;
+
+import com.employee.employeemanger.model.Employee;
+import com.employee.employeemanger.repository.EmployeeRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+public class EmployeeService {
+
+    private final EmployeeRepo employeeRepo;
+
+    @Autowired
+    public EmployeeService(EmployeeRepo employeeRepo) {
+        this.employeeRepo = employeeRepo;
+    }
+
+    public Employee createEmployee(Employee employee){
+        employee.setEmployeeCode(UUID.randomUUID().toString());
+        return employeeRepo.save(employee);
+    }
 }
