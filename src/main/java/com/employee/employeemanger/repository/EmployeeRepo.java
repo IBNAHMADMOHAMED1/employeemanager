734d75f3290ea8b3529b3a4975b9b9263c5cdcb1
IBNAHMADMOHAMED1 <ibnahamdmohamed8@gmail.com>
2022-12-06 09:57:53 +0100

    Add methode find Employee by id

--- src/main/java/com/employee/employeemanger/repository/EmployeeRepo.java
+++ src/main/java/com/employee/employeemanger/repository/EmployeeRepo.java
@@ -4,5 +4,7 @@ import com.employee.employeemanger.model.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface EmployeeRepo extends JpaRepository<Employee, Long> {
+    void deleteEmployeeById(Long id);
+    Employee findEmployeeById(Long id);
 }
 
